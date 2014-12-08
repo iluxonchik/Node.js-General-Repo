73662549f47c4ee9c937a21dd63358247bef7f39
iluxonchik <iluxon4ik@hotmail.com>
2014-12-08 13:11:10 +0000

    Server additions.

--- Node.js In Action/Chapter 2/Chat Application/server.js
+++ Node.js In Action/Chapter 2/Chat Application/server.js
@@ -5,7 +5,24 @@ var mime = require('mime'); // derive a MIME based on filename extension
 
 var cache = {}
 
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if(request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+  console.log("Server listening on port 3000.");
+})
+
 function send404(response) {
+  // NOTE: that way "404" can be sent to multiple responses
   response.writeHead(404, {'Content-Type' : 'text/plain'});
   response.write('Error 404: resource not found.');
   response.end();
@@ -16,4 +33,28 @@ function sendFile(response, filePath, fileContents) {
     {'content-type' : mime.lookup(path.basename(filePath))}
   );
   response.end(fileContents);
+}
+
+
+/* Serving Static Files */
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) { // check if the file is cached
+    sendFile(response, absPath, cache[absPath]); // serve from em
+  } else {
+    fs.exists(absPath, function(exists) {
+      if(exists) {
+        fs.readFile(absPath, function(err, dada) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data; // cache file
+            sendFile(response, absPath, data); // serve file
+          }
+        });
+      } else {
+        // File does not exist
+        send404(response);
+      }
+    })
+  }
 }
